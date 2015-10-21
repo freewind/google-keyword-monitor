28ddf0971942273f3d250986093ef9fd44ed48c2
Peng.Li <peng.li@rea-group.com>
2015-10-21 11:22:07 +0800

    basic server is OK

---
+++ server/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var alasql = require('alasql');
+var moment = require('moment');
+var _ = require('lodash');
+var sampleData = require('./sample-data');
+
+var app = express();
+
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var keywords = sampleData;
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/views/index.html');
+});
+
+app.get('/view/user-keyword', function(req, res) {
+  res.send(alasql("select who, keyword from ? \
+    group by who, keyword \
+    order by who asc, keyword asc", [keywords]));
+});
+
+app.get('/view/user-keyword-count', function(req, res) {
+  res.send(alasql("select who, keyword, count(keyword) as searchCount from ? \
+    group by who, keyword \
+    order by who asc, searchCount desc, keyword asc", [keywords]));
+});
+
+app.get('/view/user-searchEngine-keyword-count', function(req, res) {
+  res.send(alasql("select who, searchEngine, keyword, count(keyword) as searchCount from ? \
+    group by who, searchEngine, keyword \
+    order by who asc, searchEngine desc, searchCount desc, keyword asc", [keywords]));
+});
+
+app.get('/view/user-keyword-by-time', function(req, res) {
+  var result = _.map(keywords, function(row) {
+    return _.merge(row, {
+      timestamp: moment.unix(row.timestamp).format("YYYY-MM-DD hh:mm:ss")
+    })
+  })
+  res.send(keywords);
+});
+
+app.post('/keyword', function(req, res) {
+  keywords.push({
+    who: req.body.who,
+    searchEngine : req.body.searchEngine,
+    keyword : req.body.keyword,
+    timestamp : req.body.timestamp
+  })
+  res.sendStatus(201);
+});
+
+app.put('/clear', function(req, res) {
+  keywords = {};
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Google keyword monitor server listening at http://%s:%s', host, port);
+});
---
+++ server/sample-data.js
@@ -0,0 +1,32 @@
+module.exports = [
+{
+	who: "jack",
+	searchEngine: "google",
+	keyword: "aaa",
+	timestamp: 1231123
+},
+{
+	who: "lily",
+	searchEngine: "google",
+	keyword: "ccc",
+	timestamp: 1233256123
+},
+{
+	who: "lily",
+	searchEngine: "google",
+	keyword: "bbb",
+	timestamp: 1233256123
+},
+{
+	who: "jack",
+	searchEngine: "google",
+	keyword: "ddd",
+	timestamp: 12312313223
+},
+{
+	who: "lily",
+	searchEngine: "google",
+	keyword: "bbb",
+	timestamp: 1231231723
+},
+];
